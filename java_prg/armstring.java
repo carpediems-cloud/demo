fa50fc55e106ec776f60ad860a946a209dc6d65b
sahil kadukar <sahilkadukar23@gmail.com>
2026-03-31 10:37:24 +0530

    check armstrong number

---
+++ java_prg/armstring.java
@@ -0,0 +1,18 @@
+package demo.java_prg;
+import java.util.Scanner;
+public class armstring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        
+        int sum = 0;
+        for (char ch : input.toCharArray()) {
+            sum += (int) ch;
+        }
+        
+        System.out.println("Sum of ASCII values: " + sum);
+        
+        sc.close();
+    }
+}
