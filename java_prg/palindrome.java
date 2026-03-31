df8b72210a2809f4d7989f59bb99cc3a2b60eb85
sahil kadukar <sahilkadukar23@gmail.com>
2026-03-31 10:38:32 +0530

    check armstrong number

---
+++ java_prg/palindrome.java
@@ -0,0 +1,20 @@
+package demo.java_prg;
+import java.util.Scanner;
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        
+        String cleanedInput = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(cleanedInput).reverse().toString();
+        
+        if (cleanedInput.equals(reversed)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+        
+        sc.close();
+    }
+}
