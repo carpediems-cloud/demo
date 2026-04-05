94aa60e8216152a6daa11a2be48e0895488d7b46
sahil kadukar <sahilkadukar23@gmail.com>
2026-04-05 20:46:29 +0530

    Ascii value1

---
+++ java_prg/p1.java
@@ -0,0 +1,18 @@
+package demo.java_prg;
+import java.util.Scanner;
+public class p1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        
+        int sum = 0;
+        for (char ch : input.toCharArray()) {
+            sum += (int) ch;
+        }
+        
+        System.out.println("Sum of ASCII values: " + sum);
+        
+        sc.close();
+    }
+}
