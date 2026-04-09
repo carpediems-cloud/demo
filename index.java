ba5984d56f9b5a80d6bef22f66772cfd198ce67f
sahil kadukar <sahilkadukar23@gmail.com>
2026-04-09 08:37:32 +0530

    fux

--- index.java
+++ index.java
@@ -4,7 +4,7 @@ public class index {
   public static void main(String[] args){
     int num = 5;
     int fact = 1;
-    for(int i = 0; i < num; i++){
+    for(int i = 1; i <= num; i++){
       fact *= i;
     }
     System.out.println("fact " + fact);
